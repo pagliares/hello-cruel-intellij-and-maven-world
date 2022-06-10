ec98fcb106309154d99d862021f1f954728953a1
Rodrigo Martins Pagliares <pagliares@gmail.com>
2022-06-10 13:51:16 -0300

    Create IntelliJ project with Maven support with a class that outputs a welcome message to the console

---
+++ src/main/java/xyz/pagliares/HelloCruelIntelliJandMavenWorld.java
@@ -0,0 +1,7 @@
+package xyz.pagliares;
+
+public class HelloCruelIntelliJandMavenWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, cruel IntelliJ and Maven world!");
+    }
+}
